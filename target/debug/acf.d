e26486d8d8c305ef8dff8d09fd6effefcdaf4d1f
clement <clement.jvdm@gmail.com>
2024-11-29 15:42:56 +0100

    initialized project

---
+++ target/debug/acf.d
@@ -0,0 +1,1 @@
+/home/clement/projects/acf/target/debug/acf: /home/clement/projects/acf/src/main.rs
---
+++ target/debug/deps/acf-d035f674ac99e899.d
@@ -0,0 +1,5 @@
+/home/clement/projects/acf/acf/target/debug/deps/acf-d035f674ac99e899: src/main.rs
+
+/home/clement/projects/acf/acf/target/debug/deps/acf-d035f674ac99e899.d: src/main.rs
+
+src/main.rs:
